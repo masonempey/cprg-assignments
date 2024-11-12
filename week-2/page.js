0c5621de95b652228ed0b9cd177d335f568f6357
MasonJEmpey <165834350+MasonJEmpey@users.noreply.github.com>
2024-11-12 11:41:47 -0700

    assignment 2

---
+++ week-2/page.js
@@ -0,0 +1,9 @@
+import StudentInfo from "./student-info";
+
+export default function Page() {
+  return (
+    <main>
+      <StudentInfo />
+    </main>
+  );
+}
---
+++ week-2/student-info.js
@@ -0,0 +1,10 @@
+function StudentInfo() {
+  return (
+    <>
+      <h1>Mason Empey</h1>
+      <p>https://github.com/masonempey/cprg-assignments.git</p>
+    </>
+  );
+}
+
+export default StudentInfo;
